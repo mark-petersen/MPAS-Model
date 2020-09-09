214ca44af8245a76c0c8d4e3512e077139686254
Mark Petersen <mpetersen@lanl.gov>
2020-09-09 16:36:23 -0600

    update add_initial_state.py for constant T,S

--- testing_and_setup/compass/ocean/aquaplanet/QU240/Gaussian_hump/add_initial_state.py
+++ testing_and_setup/compass/ocean/aquaplanet/QU240/Gaussian_hump/add_initial_state.py
@@ -23,11 +23,6 @@ def main():
                         default='initial_state.nc',
                         help='Output file, containing initial variables'
                         )
-    parser.add_argument('-t', '--test', dest='test',
-                        default=1,
-                        type=int,
-                        help='Test number 1, 2, or 3'
-                        )
     parser.add_argument('-L', '--nVertLevels', dest='nVertLevels',
                         default=3,
                         help='Number of vertical levels'
@@ -41,10 +36,6 @@ def main():
         help='thickness of each layer, [m]')
     thicknessAllLayers = parser.parse_args().thicknessAllLayers
     nVertLevels = parser.parse_args().nVertLevels
-    test = parser.parse_args().test
-    if (test != 1) & (test != 2) & (test != 3):
-        print('Invalid choice of test. Setting to test=1.')
-        test = 1
 
     input_file = parser.parse_args().input_file
     output_file = parser.parse_args().output_file
@@ -52,7 +43,7 @@ def main():
     ds = Dataset(output_file, 'a', format='NETCDF3_64BIT_OFFSET')
 
     vertical_init(ds, thicknessAllLayers, nVertLevels)
-    tracer_init(ds, thicknessAllLayers, parser.parse_args().test)
+    tracer_init(ds, thicknessAllLayers)
     velocity_init(ds)
     coriolis_init(ds)
     others_init(ds)
@@ -106,15 +97,9 @@ def vertical_init(ds, thicknessAllLayers, nVertLevels):
 # }}}
 
 
-def tracer_init(ds, thicknessAllLayers, test):
-    K = 600.0  # kappa for Redi
-    # tracers
-    x0 = 1e2
-    P0 = 3.0e-3
-    Tx = 5 / 1.0e10
-    Ty = 0
-    Tz = 10.0 / 1000
-    S0 = 35
+def tracer_init(ds, thicknessAllLayers):
+    S0 = 35 # constant salinity in psu
+    T0 = 10 # constant temperature in C
 # {{{
     h = thicknessAllLayers
     # create new variables # {{{
@@ -128,86 +113,32 @@ def tracer_init(ds, thicknessAllLayers, test):
         'tracer2', np.float64, ('Time', 'nCells', 'nVertLevels',))
     tracer3 = ds.createVariable(
         'tracer3', np.float64, ('Time', 'nCells', 'nVertLevels',))
-    tracer1Tend = ds.createVariable(
-        'tracer1Tend', np.float64, ('Time', 'nCells', 'nVertLevels',))
-    tracer2Tend = ds.createVariable(
-        'tracer2Tend', np.float64, ('Time', 'nCells', 'nVertLevels',))
-    tracer3Tend = ds.createVariable(
-        'tracer3Tend', np.float64, ('Time', 'nCells', 'nVertLevels',))
     layerThickness = ds.variables['layerThickness']
     # }}}
 
     # obtain dimensions and mesh variables # {{{
     nVertLevels = len(ds.dimensions['nVertLevels'])
     nCells = len(ds.dimensions['nCells'])
-    xCell = ds.variables['xCell']
-    yCell = ds.variables['yCell']
+    lonCell = ds.variables['lonCell']
+    latCell = ds.variables['latCell']
     # For periodic domains, the max cell coordinate is also the domain width
-    Lx = max(xCell)
-    Ly = max(yCell)
+    Lx = max(lonCell)
+    Ly = max(latCell)
     refZMid = ds.variables['refZMid']
     refBottomDepth = ds.variables['refBottomDepth']
     H = max(refBottomDepth)
     # }}}
 
     for iCell in range(0, nCells):
-        x = xCell[iCell]
-        y = yCell[iCell]
+        x = lonCell[iCell]
+        y = latCell[iCell]
         for k in range(0, nVertLevels):
             z = refZMid[k]
 
-            # }}}
             salinity[0, iCell, k] = S0
-# TEST 1:
-#    Temperature T                  tracer P             Slope Sx                   term 1     term 2                                                      term 3
-#1   Tx*(x + x0) + Ty + Tz*z        P0*z*(x + x0)        -Tx/Tz                     0          -P0*Tx/Tz                                                   -P0*Tx/Tz
-#2   Tx*(x + x0) + Ty + Tz*z        P0*z*(x + x0)**2     -Tx/Tz                     2*P0*z     -P0*Tx*(2*x + 2*x0)/Tz                                      -P0*Tx*(2*x + 2*x0)/Tz
-#3   Tx*(x + x0) + Ty + Tz*z        P0*z**2*(x + x0)     -Tx/Tz                     0          -2*P0*Tx*z/Tz                                               -2*P0*Tx*z/Tz
-            if test==1:
-                temperature[0,iCell,k] = Tx*(x + x0) + Ty + Tz*z
-                tracer1[0,iCell,k] = P0*z*(x + x0)   
-                tracer2[0,iCell,k] = P0*z*(x + x0)**2
-                tracer3[0,iCell,k] = P0*z**2*(x + x0)
-                                           #  term 1     term 2                      term 3
-                tracer1Tend[0,iCell,k] = h*K*(0          -P0*Tx/Tz                   -P0*Tx/Tz)
-                tracer2Tend[0,iCell,k] = h*K*(2*P0*z     -P0*Tx*(2*x + 2*x0)/Tz      -P0*Tx*(2*x + 2*x0)/Tz)
-                tracer3Tend[0,iCell,k] = h*K*(0          -2*P0*Tx*z/Tz               -2*P0*Tx*z/Tz)
-# TEST 2:
-#    Temperature T                  tracer P             Slope Sx                   term 1     term 2                                                      term 3
-#5   Tx*(x + x0)**2 + Ty + Tz*z     P0*z*(x + x0)        -Tx*(2*x + 2*x0)/Tz        0          -2*P0*Tx*(x + x0)/Tz - P0*Tx*(2*x + 2*x0)/Tz                -P0*Tx*(2*x + 2*x0)/Tz
-#6   Tx*(x + x0)**2 + Ty + Tz*z     P0*z*(x + x0)**2     -Tx*(2*x + 2*x0)/Tz        2*P0*z     -2*P0*Tx*(x + x0)**2/Tz - P0*Tx*(2*x + 2*x0)**2/Tz          -P0*Tx*(2*x + 2*x0)**2/Tz
-#7   Tx*(x + x0)**2 + Ty + Tz*z     P0*z**2*(x + x0)     -Tx*(2*x + 2*x0)/Tz        0          -4*P0*Tx*z*(x + x0)/Tz - 2*P0*Tx*z*(2*x + 2*x0)/Tz          -2*P0*Tx*z*(2*x + 2*x0)/Tz
-            elif test==2:
-                temperature[0,iCell,k] = Tx*(x + x0)**2 + Ty + Tz*z
-                tracer1[0,iCell,k] = P0*z*(x + x0)   
-                tracer2[0,iCell,k] = P0*z*(x + x0)**2
-                tracer3[0,iCell,k] = P0*z**2*(x + x0)
-                                           #  term 1  term 2                                               term 3
-                tracer1Tend[0,iCell,k] = h*K*(0       -2*P0*Tx*(x + x0)/Tz - P0*Tx*(2*x + 2*x0)/Tz         -P0*Tx*(2*x + 2*x0)/Tz)
-                tracer2Tend[0,iCell,k] = h*K*(2*P0*z  -2*P0*Tx*(x + x0)**2/Tz - P0*Tx*(2*x + 2*x0)**2/Tz   -P0*Tx*(2*x + 2*x0)**2/Tz)
-                tracer3Tend[0,iCell,k] = h*K*(0       -4*P0*Tx*z*(x + x0)/Tz - 2*P0*Tx*z*(2*x + 2*x0)/Tz   -2*P0*Tx*z*(2*x + 2*x0)/Tz)
-# TEST 3:
-#    Temperature T                  tracer P             Slope Sx                   term 1     term 2                                                      term 3
-#14  Tx*(x + x0)**2 + Ty + Tz*z**2  P0*z*(x + x0)**2     -Tx*(2*x + 2*x0)/(2*Tz*z)  2*P0*z     -P0*Tx*(x + x0)**2/(Tz*z) - P0*Tx*(2*x + 2*x0)**2/(2*Tz*z)  0
-#15  Tx*(x + x0)**2 + Ty + Tz*z**2  P0*z**2*(x + x0)     -Tx*(2*x + 2*x0)/(2*Tz*z)  0          -2*P0*Tx*(x + x0)/Tz - P0*Tx*(2*x + 2*x0)/Tz                -P0*Tx*(2*x + 2*x0)/(2*Tz)
-#16  Tx*(x + x0)**2 + Ty + Tz*z**2  P0*z**2*(x + x0)**2  -Tx*(2*x + 2*x0)/(2*Tz*z)  2*P0*z**2  -2*P0*Tx*(x + x0)**2/Tz - P0*Tx*(2*x + 2*x0)**2/Tz          -P0*Tx*(2*x + 2*x0)**2/(2*Tz)
-            elif test==3:
-                temperature[0,iCell,k] = Tx*(x + x0)**2 + Ty + Tz*z**2
-                tracer1[0,iCell,k] = P0*z*(x + x0)**2
-                tracer2[0,iCell,k] = P0*z**2*(x + x0)
-                tracer3[0,iCell,k] = P0*z**2*(x + x0)**2
-                                           #  term 1     term 2                                                      term 3
-                tracer1Tend[0,iCell,k] = h*K*(2*P0*z     -P0*Tx*(x + x0)**2/(Tz*z) - P0*Tx*(2*x + 2*x0)**2/(2*Tz*z)  +0)
-                tracer2Tend[0,iCell,k] = h*K*(0          -2*P0*Tx*(x + x0)/Tz - P0*Tx*(2*x + 2*x0)/Tz                -P0*Tx*(2*x + 2*x0)/(2*Tz))
-                tracer3Tend[0,iCell,k] = h*K*(2*P0*z**2  -2*P0*Tx*(x + x0)**2/Tz - P0*Tx*(2*x + 2*x0)**2/Tz          -P0*Tx*(2*x + 2*x0)**2/(2*Tz))
-
-    # set min T to -1.8
-    temperature[:] += -1.8 - np.min(temperature[:])
-    # set all tracers to be positive, because of Redi limiter for negative tracers.
-    tracer1[:] += - np.min(tracer1[:])
-    tracer2[:] += - np.min(tracer2[:])
-    tracer3[:] += - np.min(tracer3[:])
-
+            temperature[0,iCell,k] = T0
+            tracer1[0,iCell,k] = 0.0
+            tracer2[0,iCell,k] = 0.0
 
 def velocity_init(ds):
     # {{{
