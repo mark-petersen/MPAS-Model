7cb673a6c89ce6a4df2876699f93eb83b715ee67
Mark Petersen <mpetersen@lanl.gov>
2020-09-30 08:51:02 -0600

    Changing to convergence test only

--- testing_and_setup/compass/ocean/verification/barotropic_cartesian/coastal_kelvin_wave/create_meshes.py
+++ testing_and_setup/compass/ocean/verification/barotropic_cartesian/coastal_kelvin_wave/create_meshes.py
@@ -11,8 +11,8 @@ import math
 
 # replace with flags later
 nCellsXMin = 16
-nCellsXMax = 64
-nCellsFactor = 2
+nCellsXMax = 32
+nCellsFactor = math.sqrt(2)
 domainWidthX = 100.0*50000.0
 
 nCases = int(round(math.log(nCellsXMax,nCellsFactor) - math.log(nCellsXMin,nCellsFactor) + 1))
@@ -25,16 +25,15 @@ for iCase in range(nCases-1):
     # ensure nCellsX is an even integer
     nCellsX[iCase+1] = int(nCellsFloat/2+0.01)*2
 
-for iCase in range(nCases):
-    dcEdge = domainWidthX/float(nCellsX[iCase])
-    os.system("planar_hex.py --nx %d --ny %d --npx --dc %f -o base_mesh_%d.nc" 
-              %(nCellsX[iCase],nCellsX[iCase],dcEdge,nCellsX[iCase]))
-    os.system("MpasCellCuller.x base_mesh_%d.nc culled_mesh_%d.nc" 
-              %(nCellsX[iCase],nCellsX[iCase]))
-    os.system("MpasMeshConverter.x culled_mesh_%d.nc mesh_%d.nc" 
-              %(nCellsX[iCase],nCellsX[iCase]))
-    os.system("mv culled_graph.info culled_graph_%d.info" %(nCellsX[iCase]))
-    os.system("mv graph.info graph_%d.info" %(nCellsX[iCase]))
+print('nCellsX:',nCellsX)
 
-os.system("rm culled_graph_*.info")
-os.system("rm graph_*.info")
+for iCase in range(nCases):
+    nxDir = 'nx' + str(nCellsX[iCase]).zfill(4)
+    os.system("mkdir %s"%(nxDir))
+    os.chdir(nxDir)
+    dcEdge = domainWidthX/float(nCellsX[iCase])
+    os.system("planar_hex --nx %d --ny %d --npx --dc %f -o base_mesh.nc" 
+              %(nCellsX[iCase],nCellsX[iCase],dcEdge))
+    os.system("MpasCellCuller.x base_mesh.nc culled_mesh.nc")
+    os.system("MpasMeshConverter.x culled_mesh.nc mesh.nc")
+    os.chdir("..")
