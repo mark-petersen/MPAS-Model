5a256df67e88c9d0fe8c9290402f884c5aeb5385
Mark Petersen <mpetersen@lanl.gov>
2020-11-06 18:48:00 -0700

    Simulation runs. Solution looks funny

--- testing_and_setup/compass/ocean/verification/coastal_kelvin_wave/res1/plot.py
+++ testing_and_setup/compass/ocean/verification/coastal_kelvin_wave/res1/plot.py
@@ -15,9 +15,9 @@ nx = ncfileMesh.getncattr('nx')
 ny = ncfileMesh.getncattr('ny')
 iz = 1
 iTime=0
+nGrids = 3
 
 ncfile = Dataset('output.nc', 'r')
-var = np.reshape(ncfile.variables['ssh'][0, :], [ny, nx])
 #var = np.reshape(ncfile.variables['ssh'][0, :, iz], [ny, nx])
 
 #dif = abs(var[1:ny - 1, 1:nx - 2] - sol[1:ny - 1, 1:nx - 2])
@@ -27,17 +27,20 @@ var = np.reshape(ncfile.variables['ssh'][0, :], [ny, nx])
 #errL2[i, j, k] = np.sqrt(np.mean(err[:]**2))
 
 # --- Every other row in y needs to average two neighbors in x on planar hex mesh
-var_avg = var
-#sol_avg = sol
-for iy in range(0, ny, 2):
-    for ix in range(1, nx - 2):
-        var_avg[iy, ix] = (var[iy, ix + 1] + var[iy, ix]) / 2.0
-        #sol_avg[iy, ix] = (sol[iy, ix + 1] + sol[iy, ix]) / 2.0
-
-#ax1 = plt.subplot(nGrids, 3, 3 * j + 1)
-plt.imshow(var_avg)
-plt.colorbar()
-#plt.title(test + ' ' + tracer + ' ' + grid + ' computed')
+for j in range(3):
+   var = np.reshape(ncfile.variables['ssh'][j, :], [ny, nx])
+   var_avg = var
+   #sol_avg = sol
+   for iy in range(0, ny, 2):
+       for ix in range(1, nx - 2):
+           var_avg[iy, ix] = (var[iy, ix + 1] + var[iy, ix]) / 2.0
+           #sol_avg[iy, ix] = (sol[iy, ix + 1] + sol[iy, ix]) / 2.0
+   
+   ax1 = plt.subplot(nGrids, 3, 3 * j + 1)
+   plt.imshow(var_avg)
+   plt.colorbar()
+   plt.set_cmap('bwr')
+   plt.title('time = '+str(j))
 #
 #ax2 = plt.subplot(nGrids, 3, 3 * j + 2)
 #plt.imshow(sol_avg[1:ny - 1, 1:nx - 2])
