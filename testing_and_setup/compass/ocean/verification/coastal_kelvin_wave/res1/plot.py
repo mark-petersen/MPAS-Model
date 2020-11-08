98b648c0e22b07bb4d629f8a1c1601e0fe48ec65
Mark Petersen <mpetersen@lanl.gov>
2020-11-08 10:29:15 -0700

    Simulation and solution in place. Added yDomainWidth

--- testing_and_setup/compass/ocean/verification/coastal_kelvin_wave/res1/plot.py
+++ testing_and_setup/compass/ocean/verification/coastal_kelvin_wave/res1/plot.py
@@ -26,28 +26,29 @@ ncfile = Dataset('output.nc', 'r')
 #difL2[i, j, k] = np.sqrt(np.mean(dif[:]**2))
 #errL2[i, j, k] = np.sqrt(np.mean(err[:]**2))
 
-# --- Every other row in y needs to average two neighbors in x on planar hex mesh
-for j in range(3):
-   var = np.reshape(ncfile.variables['ssh'][j, :], [ny, nx])
-   var_avg = var
-   #sol_avg = sol
-   for iy in range(0, ny, 2):
-       for ix in range(1, nx - 2):
-           var_avg[iy, ix] = (var[iy, ix + 1] + var[iy, ix]) / 2.0
-           #sol_avg[iy, ix] = (sol[iy, ix + 1] + sol[iy, ix]) / 2.0
-   
-   ax1 = plt.subplot(nGrids, 3, 3 * j + 1)
-   plt.imshow(var_avg)
-   plt.colorbar()
-   plt.set_cmap('bwr')
-   plt.title('time = '+str(j))
+varNames = ['ssh','sshSolution']
+for iTime in range(3):
+    for iVar, varName in enumerate(varNames):
+      var = np.reshape(ncfile.variables[varName][iTime, :], [ny, nx])
+      var_avg = var
+      #sol_avg = sol
+      for iy in range(0, ny, 2):
+          for ix in range(1, nx - 2):
+              var_avg[iy, ix] = (var[iy, ix + 1] + var[iy, ix]) / 2.0
+              #sol_avg[iy, ix] = (sol[iy, ix + 1] + sol[iy, ix]) / 2.0
+      
+      ax1 = plt.subplot(3, 4, 4 * iTime + iVar + 1)
+      plt.imshow(var_avg)
+      plt.colorbar()
+      plt.set_cmap('bwr')
+      plt.title(varName + 'time = '+str(iTime))
 #
-#ax2 = plt.subplot(nGrids, 3, 3 * j + 2)
+#ax2 = plt.subplot(nGrids, 3, 3 * iTime + 2)
 #plt.imshow(sol_avg[1:ny - 1, 1:nx - 2])
 #plt.colorbar()
 #plt.title(test + ' ' + tracer + ' ' + grid + ' solution')
 #
-#ax3 = plt.subplot(nGrids, 3, 3 * j + 3)
+#ax3 = plt.subplot(nGrids, 3, 3 * iTime + 3)
 #plt.imshow(var_avg[1:ny - 1, 1:nx - 2] - sol_avg[1:ny - 1, 1:nx - 2])
 #plt.colorbar()
 #plt.title(test + ' ' + tracer + ' ' + grid + ' error')
