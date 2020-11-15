01b8f0e6461fa0cd75af9160540680c5c7b9232c
Mark Petersen <mpetersen@lanl.gov>
2020-11-15 16:51:09 -0700

    add semi-implicit and split explicit to test

--- testing_and_setup/compass/ocean/verification/scripts/compute_dx_dt.py
+++ testing_and_setup/compass/ocean/verification/scripts/compute_dx_dt.py
@@ -7,12 +7,14 @@ nx1=50
 dx1 = lx/nx1
 
 dt = np.zeros(6)
+btr_dt = np.zeros(6)
 dx = np.zeros(6)
 
-print('j    nx   dx   nx*dx   dt')
+print('j    nx   dx   nx*dx   dt   btr_dt')
 for j in range(len(nx)):
     dx[j] = lx / nx[j]
     dt[j] = dt1 *dx[j] / dx1
-    print(j,nx[j],dx[j],nx[j]*dx[j],dt[j])
+    btr_dt[j] = dt[j] / 20.0
+    print(j,nx[j],dx[j],nx[j]*dx[j],dt[j], btr_dt[j])
 
 
--- testing_and_setup/compass/ocean/verification/scripts/plot_convergence.py
+++ testing_and_setup/compass/ocean/verification/scripts/plot_convergence.py
@@ -12,6 +12,8 @@ from scipy.interpolate import RegularGridInterpolator
 matplotlib.use('Agg')
 
 varNames = ['ssh'] #,'normalVelocity']
+time_step_method = 'split_explicit'
+time_step_method = 'RK4'
 #varNames = ['ssh','normalVelocity']
 nVars = len(varNames)
 plotDir = os.getcwd()
@@ -20,16 +22,11 @@ nGrid = 10
 
 difL1 = np.zeros([nRes,nVars])
 difL2 = np.zeros([nRes,nVars])
-
-os.chdir('../../res1/simulation')
-#ncfileMesh = Dataset('initial_state.nc', 'r')
-#ncfileMesh.close()
-
 dx = np.zeros([nRes])
 
 iTime = 0
 for iRes in range(nRes):
-   os.chdir('../../res'+str(iRes+1)+'/simulation')
+   os.chdir('../../resolution'+str(iRes+1)+'/simulation_'+ time_step_method)
    ncfileHex = Dataset('planar_hex.nc', 'r')
    nx = ncfileHex.getncattr('nx')
    ny = ncfileHex.getncattr('ny')
@@ -57,28 +54,31 @@ for iRes in range(nRes):
    for iVar, varName in enumerate(varNames):
        var = np.reshape( ncfile.variables[varName][iTime, :], [ny, nx])
        sol = np.reshape( ncfile.variables[varName+'Solution'][iTime, :], [ny, nx])
-       # --- Every other row in y needs to average two neighbors in x on planar hex mesh
-       varGrid = np.zeros([ny,2*nx-2])
-       solGrid = np.zeros([ny,2*nx-2])
-       for iy in range(0, ny, 2):
-           for ix in range(0, nx-1):
-               varGrid[iy, 2*ix] = (var[iy, ix] + var[iy, ix+1]) / 2.0
-               varGrid[iy, 2*ix+1] = var[iy, ix+1]
-               varGrid[iy+1, 2*ix] = var[iy+1, ix]
-               varGrid[iy+1, 2*ix+1] = (var[iy+1, ix] + var[iy+1, ix+1]) / 2.0
-               solGrid[iy, 2*ix] = (sol[iy, ix] + sol[iy, ix+1]) / 2.0
-               solGrid[iy, 2*ix+1] = sol[iy, ix+1]
-               solGrid[iy+1, 2*ix] = sol[iy+1, ix]
-               solGrid[iy+1, 2*ix+1] = (sol[iy+1, ix] + sol[iy+1, ix+1]) / 2.0
-       if iRes==0:
-           dif = varGrid-solGrid
+       error_on_same_grid = True
+
+       if (error_on_same_grid):
+           varGrid = np.zeros([ny,2*nx-2])
+           solGrid = np.zeros([ny,2*nx-2])
+           for iy in range(0, ny, 2):
+               for ix in range(0, nx-1):
+                   varGrid[iy, 2*ix] = (var[iy, ix] + var[iy, ix+1]) / 2.0
+                   varGrid[iy, 2*ix+1] = var[iy, ix+1]
+                   varGrid[iy+1, 2*ix] = var[iy+1, ix]
+                   varGrid[iy+1, 2*ix+1] = (var[iy+1, ix] + var[iy+1, ix+1]) / 2.0
+                   solGrid[iy, 2*ix] = (sol[iy, ix] + sol[iy, ix+1]) / 2.0
+                   solGrid[iy, 2*ix+1] = sol[iy, ix+1]
+                   solGrid[iy+1, 2*ix] = sol[iy+1, ix]
+                   solGrid[iy+1, 2*ix+1] = (sol[iy+1, ix] + sol[iy+1, ix+1]) / 2.0
+           if iRes==0:
+               dif = varGrid-solGrid
+           else:
+               varFxn = RegularGridInterpolator((yCell1D, xCell1D), varGrid)
+               solFxn = RegularGridInterpolator((yCell1D, xCell1D), solGrid)
+               varInterp = varFxn((YGridRef, XGridRef))
+               solInterp = solFxn((YGridRef, XGridRef))
+               dif = varInterp - solInterp
        else:
-           varFxn = RegularGridInterpolator((yCell1D, xCell1D), varGrid)
-           solFxn = RegularGridInterpolator((yCell1D, xCell1D), solGrid)
-           varInterp = varFxn((YGridRef, XGridRef))
-           solInterp = solFxn((YGridRef, XGridRef))
-           #solInterp = solFxn((yCell1Dref, xCell1Dref))
-           dif = varInterp - solInterp
+           dif = var - sol
 
        difL1[iRes,iVar] = np.max(abs(dif[:]))
        difL2[iRes,iVar] = np.sqrt(np.mean(dif[:]**2))
@@ -93,6 +93,18 @@ print('ssh diffL2', difL2)
 print('dx[i+1]/dx[i], (dx[i+1]/dx[i])^2, difL1[i+1]/difL1[i], difL2[i+1]/difL2[i], L1Slope, L2Slope')
 for i in range(nRes-1):
     print(dx[i+1]/dx[i], (dx[i+1]/dx[i])**2, difL1[i+1]/difL1[i], difL2[i+1]/difL2[i], difL1[i+1]/difL1[i]/(dx[i+1]/dx[i]), difL2[i+1]/difL2[i]/(dx[i+1]/dx[i]))
+
+p = np.polyfit(np.log10(dx),np.log10(difL2),1)
+conv = abs(p[0])
+
+yfit = dx**p[0]*10**p[1]
+
+plt.loglog(dx,yfit,'k')
+plt.loglog(dx,difL2,'or')
+plt.annotate('Order of Convergence = {}'.format(np.round(conv,2)),xycoords='axes fraction',xy=(0.4,0.95),fontsize=14)
+plt.xlabel('Number of Grid Cells',fontsize=14)
+plt.ylabel('L2 Norm',fontsize=14)
+plt.savefig('convergence.png',bbox_inches='tight', pad_inches=0.1)
 
 # ax1 = plt.subplot(3, 3, 3 * j + 0 + 1)
 # #plt.imshow(var_avg)
